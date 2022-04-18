bab771c85cded8cdea94e9cf2f00701aa869b13d
Alpha <alpha6662017@outlook.com>
2022-04-18 21:00:19 +0800

    [Reward] 模組化完成

--- module/Reward.py
+++ module/Reward.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup as BS
 from colorama import Fore
 
 from cross import *
+from substances import *
 
 def Reward_M():
 
@@ -51,168 +52,48 @@ def Reward_M():
         print(Fore.RED+'\n[WARNING]This tool is ONLY add for offical data. If you want to add custom data likes ddsMap or chara, etc. Please add custom data by yourself.')
 
         #gamepoint reward
-        if _iType==0:
-            _sAmount=input(Fore.GREEN+'[INFO]Enter amount of gamePoint that you want to add:')
-        else:
-            _sAmount='0'
-        _tGamepoint=tagM('<gamePoint><gamePoint>'+_sAmount+'</gamePoint></gamePoint>')
+        _tGamepoint=SubstanceTagMaker('Reward','gamePoint',_iType)
         _dXml.RewardData.substances.list.RewardSubstanceData.append(_tGamepoint)
 
-
         #ticket
-        if _iType==1:
-            _sTicketId=input(Fore.GREEN+'[INFO]Enter id of ticket that you want to give:')
-            if checkconfig('AutoStr','Ticket'):
-                _fFInd,_sTicketStr=getstr('name','AutoStr','Ticket',_sTicketId)
-                if not _fFInd:
-                    return
-            else:
-                _sTicketStr=input(Fore.GREEN+'[INFO]Enter str of ticket that you want to give:')
-        else:
-            _sTicketId='-1'
-            _sTicketStr='Invalid'
-        _tTicket=tagM('<ticket><ticketName><id>'+_sTicketId+'</id><str>'+_sTicketStr+'</str><data></ticketName></ticket>')
+        _tTicket=SubstanceTagMaker('Reward','ticket',_iType)
         _dXml.RewardData.substances.list.RewardSubstanceData.append(_tTicket)
         
         #trophy
-        if _iType==2:
-            _sTrophyId=input(Fore.GREEN+'[INFO]Enter id of trophy that you want to give:')
-            if checkconfig('AutoStr','Trophy'):
-                _fFInd,_sTrophyStr=getstr('name','AutoStr','Trophy',_sTrophyId)
-                if not _fFInd:
-                    return
-            else:
-                _sTrophyStr=input(Fore.GREEN+'[INFO]Enter str of trophy that you want to give:')
-        else:
-            _sTrophyId='-1'
-            _sTrophyStr='Invalid'
-        
-        _tTrophy=tagM('<trophy><trophyName><id>'+_sTrophyId+'</id><str>'+_sTrophyStr+'</str><data></trophyName></trophy>')
+        _tTrophy=SubstanceTagMaker('Reward','trophy',_iType)        
         _dXml.RewardData.substances.list.RewardSubstanceData.append(_tTrophy)
         
         #chara
-        if _iType==3:
-            _sCharaId=input(Fore.GREEN+'[INFO]Enter id of chara that you want to give:')
-            if checkconfig('AutoStr','Chara'):
-                _fFInd,_sCharaStr=getstr('name','AutoStr','Chara',_sCharaId)
-                if not _fFInd:
-                    return
-            else:
-                _sCharaStr=input(Fore.GREEN+'[INFO]Enter str of chara that you want to give:')
-        else:
-            _sCharaId='-1'
-            _sCharaStr='Invalid'
-        _tChara=tagM('<chara><charaName><id>'+_sCharaId+'</id><str>'+_sCharaStr+'</str><data></charaName></chara>')
+        _tChara=SubstanceTagMaker('Reward','chara',_iType)
         _dXml.RewardData.substances.list.RewardSubstanceData.append(_tChara)
 
         #Skillseed
-        if _iType==4:
-            _sSkillseedId=input(Fore.GREEN+'[INFO]Enter id of skillseed that you want to give:')
-            _sSkillseedcount=input(Fore.GREEN+'[INFO]Enter count of skillseed that you want to give:')
-            if checkconfig('AutoStr','Skill'):
-                _fFInd,_sSkillseedStr=getstr('name','AutoStr','Skill',_sSkillseedId)
-                if not _fFInd:
-                    return
-            else:
-                _sSkillseedStr=input(Fore.GREEN+'[INFO]Enter str of skillseed that you want to give:')
-        else:
-            _sSkillseedId='-1'
-            _sSkillseedStr='Invalid'
-            _sSkillseedcount='1'
-        _tSkillseed=tagM('<skillSeed><skillSeedName><id>'+_sSkillseedId+'</id><str>'+_sSkillseedStr+'</str><data></skillSeedName></skillSeed>')
+        _tSkillseed,_tSkillseedcount=SubstanceTagMaker('Reward','skillSeed',_iType)
         _dXml.RewardData.substances.list.RewardSubstanceData.append(_tSkillseed)
-        _tSkillseedcount=tagM('<skillSeedCount>'+_sSkillseedcount+'</skillSeedCount>')
-        _dXml.RewardData.substances.list.RewardSubstanceData.skillSeed.append(_tSkillseedcount)
-        
+        _dXml.RewardData.substances.list.RewardSubstanceData.skillSeed.append(_tSkillseedcount) 
 
         #Nameplate
-        if _iType==5:
-            _sNameplateId=input(Fore.GREEN+'[INFO]Enter id of nameplate that you want to give:')
-            if checkconfig('AutoStr','Nameplate'):
-                _fFInd,_sNameplateStr=getstr('name','AutoStr','Nameplate',_sNameplateId)
-                if not _fFInd:
-                    return
-            else:
-                _sNameplateStr=input(Fore.GREEN+'[INFO]Enter str of nameplate that you want to give:')
-        else:
-            _sNameplateId='-1'
-            _sNameplateStr='Invalid'
-        _tNameplate=tagM('<namePlate><namePlateName><id>'+_sNameplateId+'</id><str>'+_sNameplateStr+'</str><data></namePlateName></namePlate>')
+        _tNameplate=SubstanceTagMaker('Reward','namePlate',_iType)
         _dXml.RewardData.substances.list.RewardSubstanceData.append(_tNameplate)
 
         #Music
-        if _iType==6:
-            _sMusicId=input(Fore.GREEN+'[INFO]Enter id of music that you want to give:')
-            if checkconfig('AutoStr','Music'):
-                _fFInd,_sMusicStr=getstr('name','AutoStr','Music',_sMusicId)
-                if not _fFInd:
-                    return
-            else:
-                _sMusicStr=input(Fore.GREEN+'[INFO]Enter str of music that you want to give:')
-        else:
-            _sMusicId='-1'
-            _sMusicStr='Invalid'
-        _tMusic=tagM('<music><musicName><id>'+_sMusicId+'</id><str>'+_sMusicStr+'</str><data></musicName></music>')
+        _tMusic=SubstanceTagMaker('Reward','music',_iType)
         _dXml.RewardData.substances.list.RewardSubstanceData.append(_tMusic)
         
         #Mapicon
-        if _iType==7:
-            _sMapiconId=input(Fore.GREEN+'[INFO]Enter id of mapicon that you want to give:')
-            if checkconfig('AutoStr','Mapicon'):
-                _fFInd,_sMapiconStr=getstr('name','AutoStr','Mapicon',_sMapiconId)
-                if not _fFInd:
-                    return
-            else:
-                _sMapiconStr=input(Fore.GREEN+'[INFO]Enter str of mapicon that you want to give:')
-        else:
-            _sMapiconId='-1'
-            _sMapiconStr='Invalid'
-        _tMapicon=tagM('<mapIcon><mapIconName><id>'+_sMapiconId+'</id><str>'+_sMapiconStr+'</str><data></mapIconName></mapIcon>')
+        _tMapicon=SubstanceTagMaker('Reward','mapIcon',_iType)
         _dXml.RewardData.substances.list.RewardSubstanceData.append(_tMapicon)
 
         #Systemvoice
-        if _iType==8:
-            _sSystemvoiceId=input(Fore.GREEN+'[INFO]Enter id of systemvoice that you want to give:')
-            if checkconfig('AutoStr','Systemvoice'):
-                _fFInd,_sSystemvoiceStr=getstr('name','AutoStr','Systemvoice',_sSystemvoiceId)
-                if not _fFInd:
-                    return
-            else:
-                _sSystemvoiceStr=input(Fore.GREEN+'[INFO]Enter str of systemvoice that you want to give:')
-        else:
-            _sSystemvoiceId='-1'
-            _sSystemvoiceStr='Invalid'
-        _tSystemvoice=tagM('<systemVoice><systemVoiceName><id>'+_sSystemvoiceId+'</id><str>'+_sSystemvoiceStr+'</str><data></systemVoiceName></systemVoice>')
+        _tSystemvoice=SubstanceTagMaker('Reward','systemVoice',_iType)
         _dXml.RewardData.substances.list.RewardSubstanceData.append(_tSystemvoice)
 
         #AvatarAccessory
-        if _iType==9:
-            _sAvatarAccessoryId=input(Fore.GREEN+'[INFO]Enter id of avataraccessory that you want to give:')
-            if checkconfig('AutoStr','AvatarAccessory'):
-                _fFInd,_sAvatarAccessoryStr=getstr('name','AutoStr','AvatarAccessory',_sAvatarAccessoryId)
-                if not _fFInd:
-                    return
-            else:
-                _sAvatarAccessoryStr=input(Fore.GREEN+'[INFO]Enter str of avataraccessory that you want to give:')
-        else:
-            _sAvatarAccessoryId='-1'
-            _sAvatarAccessoryStr='Invalid'
-        _tAvaterAccessory=tagM('<avatarAccessory><avatarAccessoryName><id>'+_sAvatarAccessoryId+'</id><str>'+_sAvatarAccessoryStr+'</str><data></avatarAccessoryName></avatarAccessory>')
+        _tAvaterAccessory=SubstanceTagMaker('Reward','avatarAccessory',_iType)
         _dXml.RewardData.substances.list.RewardSubstanceData.append(_tAvaterAccessory)
 
         #Frame
-        if _iType==10:
-            _sFrameId=input(Fore.GREEN+'[INFO]Enter id of frame that you want to give:')
-            if checkconfig('AutoStr','Frame'):
-                _fFInd,_sFrameStr=getstr('name','AutoStr','Frame',_sFrameId)
-                if not _fFInd:
-                    return
-            else:
-                _sFrameStr=input(Fore.GREEN+'[INFO]Enter str of frame that you want to give:')
-        else:
-            _sFrameId='-1'
-            _sFrameStr='Invalid'
-        _tFrame=tagM('<frame><frameName><id>'+_sFrameId+'</id><str>'+_sFrameStr+'</str><data></frameName></frame>')
+        _tFrame=SubstanceTagMaker('Reward','frame',_iType)
         _dXml.RewardData.substances.list.RewardSubstanceData.append(_tFrame)
 
 
