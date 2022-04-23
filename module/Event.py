fb0491536cb1355da667229d961b543368c4dd54
Alpha <alpha6662017@outlook.com>
2022-04-23 18:07:33 +0800

    [Event] XML生成及輸出功能完成

---
+++ module/Event.py
@@ -0,0 +1,85 @@
+from colorama import Fore
+
+from cross import *
+
+def EventM():
+
+    #建立Xml資料
+    _dXml=tagM('')
+
+    #add data title
+    _tXmlVer=tagM('<?xml version="1.0" encoding="utf-8"?>')
+    _tTitle=tagM('<EventData xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"></EventData>')
+    _dXml.append(_tXmlVer)
+    _dXml.append(_tTitle)
+
+    #add dataname ,name and netOpenName tags
+    _tNetOpenName=tagM('<netOpenName><id>2100</id><str>v2_05 00_0</str><data /></netOpenName>')
+    _sEventNum=input(Fore.GREEN+'[INFO]Enter custom event num:'+Fore.RESET)
+    _sEventStr=input(Fore.GREEN+'[INFO]Enter custom event str:'+Fore.RESET)
+    if not _sEventStr.endswith('  |マップフラグ1'):
+        _sEventStr=_sEventStr+'  |マップフラグ1'
+    _tName=tagM('<name><id>'+_sEventNum+'</id><str>'+_sEventStr+'</str><data/></name>')
+    if len(_sEventNum)>8:
+        print(Fore.RED+'[ERROR] Data number should be less than 8 digits!'+Fore.RESET)
+        os.system('PAUSE')
+        return
+    while len(_sEventNum)!=8:
+        _sEventNum='0'+_sEventNum
+    _tDataName=tagM('<dataName>mapArea'+_sEventNum+'</dataName>')
+    _dXml.EventData.append(_tDataName)
+    _dXml.EventData.append(_tNetOpenName)
+    _dXml.EventData.append(_tName)
+
+    #ddsBannerTag
+    if input(Fore.GREEN+'[INFO]Do you want to add ddsBanner?(y/n)'+Fore.RESET).lower()=='y':
+        _sDdsBannerId=input(Fore.GREEN+'[INFO]Enter id of ddsbanner.'+Fore.RESET)
+        if checkconfig('AutoStr','ddsBanner'):
+            _fFInd,_sDdsBannerStr=getstr('name','AutoStr','ddsBanner',_sDdsBannerId)
+            if not _fFInd:
+                return
+        else:
+            _sDdsBannerStr=input(Fore.GREEN+'[INFO] Enter the ddsBanner Str.'+Fore.RESET)
+    else:
+        _sDdsBannerId='-1'
+        _sDdsBannerStr='Invalid'
+    _dXml.EventData.append(tagM('<ddsBannerName><id>'+_sDdsBannerId+'</id><str>'+_sDdsBannerStr+'</str><data /></ddsBannerName>'))
+
+    #OtherTag add
+    _dXml.EventData.append(tagM('<periodDispType>1</periodDispType>'))
+    _dXml.EventData.append(tagM('<alwaysOpen>true</alwaysOpen>'))
+    _dXml.EventData.append(tagM('<teamOnly>false</teamOnly>'))
+    _dXml.EventData.append(tagM('<isKop>false</isKop>'))
+    _dXml.EventData.append(tagM('<priority>0</priority>'))
+
+    #substances tag
+    _sMapId=input(Fore.GREEN+'[INFO]Enter id of map:'+Fore.RESET)
+    if checkconfig('AutoStr','map'):
+        _fFInd,_sMapStr=getstr('name','AutoStr','map',_sDdsBannerId)
+        if not _fFInd:
+            return
+    else:
+        _sMapStr=input(Fore.GREEN+'[INFO] Enter str of map:'+Fore.RESET)
+    _tSubstamces=tagM('<substances><type>2</type><flag><value>0</value></flag><information><informationType>0</informationType><informationDispType>0</informationDispType><mapFilterID><id>-1</id><str>Invalid</str><data /></mapFilterID><courseNames><list /></courseNames><text /><image><path /></image><movieName><id>-1</id><str>Invalid</str><data /></movieName><presentNames><list /></presentNames></information><map><tagText /><mapName><id>'+_sMapId+'</id><str>'+_sMapStr+'</str><data /></mapName><musicNames><list /></musicNames></map><music><musicType>0</musicType><musicNames><list /></musicNames></music><advertiseMovie><firstMovieName><id>-1</id><str>Invalid</str><data /></firstMovieName><secondMovieName><id>-1</id><str>Invalid</str><data /></secondMovieName></advertiseMovie><recommendMusic><musicNames><list /></musicNames></recommendMusic><release><value>0</value></release><course><courseNames><list /></courseNames></course><quest><questNames><list /></questNames></quest><duel><duelName><id>-1</id><str>Invalid</str><data /></duelName></duel><changeSurfBoardUI><value>0</value></changeSurfBoardUI><avatarAccessoryGacha><avatarAccessoryGachaName><id>-1</id><str>Invalid</str><data /></avatarAccessoryGachaName></avatarAccessoryGacha><rightsInfo><rightsNames><list /></rightsNames></rightsInfo><battleReward><battleRewardName><id>-1</id><str>Invalid</str><data /></battleRewardName></battleReward></substances>')
+    _dXml.EventData.append(_tSubstamces)
+
+    #print(_dXml.prettify())
+    if not checkconfig('SavePath','EventPath'):
+        print(Fore.RED+'[ERROR] You didn\'t enter save path for Event!'+Fore.RESET)
+        os.system('PAUSE')
+        return
+    else:
+        _sSavePath=getconfig('SavePath','EventPath')+'/Event'+_sEventNum
+        if not os.path.isdir(_sSavePath):
+            os.mkdir(_sSavePath)
+        
+        with open(_sSavePath+'/Event.xml','w',encoding='utf-8')as f:
+            f.write(str(_dXml))
+            f.close()
+
+        XMLFormat(_sSavePath+'/Event.xml')
+        print(Fore.GREEN+'\nFinish!!!'+Fore.RESET)
+        os.system('PAUSE')
+
+if __name__=='__main__':
+    EventM()
