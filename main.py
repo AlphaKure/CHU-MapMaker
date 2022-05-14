5aa938db56bd6d57cfebf948b1af47eb61e7bffd
Alpha <alpha6662017@outlook.com>
2022-05-14 19:24:03 +0800

    [main] 讀取文本移到無窮迴圈外

--- main.py
+++ main.py
@@ -16,39 +16,37 @@ def GetContent(lang):
 
 def Main():
 
+    os.system('cls')
+    #content read
+    Content=GetContent(module.cross.getconfig('lang','lang'))
+    if Content=='':
+        os.system('pause')
+        return
     while True:
-        os.system('cls')
-        #content read
-        Content=GetContent(module.cross.getconfig('lang','lang'))
-        if Content=='':
+        #logo
+        print(pyfiglet.figlet_format("CHU-MapMaker",font="slant"))
+        print(Fore.GREEN+Content['Cross']['Menu']['Welcome']+'\n'+Fore.RESET)
+        print(Fore.GREEN+Content['Cross']['Menu']['Title']+Fore.RESET)
+        print(Fore.GREEN+Content['Cross']['Menu']['Help']+Fore.RESET)
+        for i in range(0,6):
+            print(Fore.GREEN+Content['Cross']['Menu']['Type_'+str(i)]+Fore.RESET)
+        Cmd=int(input(Fore.GREEN+'>'+Fore.RESET))
+        if Cmd>5:
+            continue
+        elif Cmd==5:
+            print(Fore.GREEN+'[INFO] See you next time!'+Fore.RESET)
             os.system('pause')
             return
-
-        else:
-            #logo
-            print(pyfiglet.figlet_format("CHU-MapMaker",font="slant"))
-            print(Fore.GREEN+Content['Cross']['Menu']['Welcome']+'\n'+Fore.RESET)
-            print(Fore.GREEN+Content['Cross']['Menu']['Title']+Fore.RESET)
-            print(Fore.GREEN+Content['Cross']['Menu']['Help']+Fore.RESET)
-            for i in range(0,6):
-                print(Fore.GREEN+Content['Cross']['Menu']['Type_'+str(i)]+Fore.RESET)
-            Cmd=int(input(Fore.GREEN+'>'+Fore.RESET))
-            if Cmd>5:
-                continue
-            elif Cmd==5:
-                print(Fore.GREEN+'[INFO] See you next time!'+Fore.RESET)
-                os.system('pause')
-                return
-            elif Cmd==0:
-                module.Reward.RewardM(Content)
-            elif Cmd==1:
-                module.MapBonus.MapBonusM(Content)
-            elif Cmd==2:
-                module.MapArea.MapAreaM(Content)
-            elif Cmd==3:
-                module.Map.MapM(Content)
-            elif Cmd==4:
-                module.Event.EventM(Content)
+        elif Cmd==0:
+            module.Reward.RewardM(Content)
+        elif Cmd==1:
+            module.MapBonus.MapBonusM(Content)
+        elif Cmd==2:
+            module.MapArea.MapAreaM(Content)
+        elif Cmd==3:
+            module.Map.MapM(Content)
+        elif Cmd==4:
+            module.Event.EventM(Content)
 
 
 
