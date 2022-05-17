1272d544355f84b7750d68f19e0737a3ea6eca12
Alpha <alpha6662017@outlook.com>
2022-05-17 22:53:14 +0800

    [substances] 文本顯示功能完成

--- module/substances.py
+++ module/substances.py
@@ -1,3 +1,4 @@
+from asyncio.windows_events import NULL
 from bs4 import BeautifulSoup
 from colorama import Fore
 
@@ -6,6 +7,8 @@ from module.cross import *
 def SubstanceTagMaker(FileType:str,NowPart:str,Type:int)->BeautifulSoup:
     #Type為使用者選擇的值，假設使用者選擇5，為了生成其他選項的空值TAG，假設到6的地方由於輸入Type不同，即可自行生成空值TAG
 
+    global Content #文本
+
     #Reward substances
     if FileType=='Reward':
         TagNameList=['','ticket','trophy','chara','','namePlate','music','mapIcon','systemVoice','avatarAccessory','frame'] #不讓gamePoint skillSeed 碰撞
@@ -13,7 +16,7 @@ def SubstanceTagMaker(FileType:str,NowPart:str,Type:int)->BeautifulSoup:
         #gamePoint-reward
         if NowPart=='gamePoint':
             if Type==0:
-                _sAmount=input(Fore.GREEN+'[INFO]Enter amount of gamePoint that you want to add:'+Fore.RESET)
+                _sAmount=input(Fore.GREEN+Content['Cross']['Input']['Input_GamePoint']+Fore.RESET)
             else:
                 _sAmount='0'
             _tGamepoint=tagM('<gamePoint><gamePoint>'+_sAmount+'</gamePoint></gamePoint>')
@@ -22,14 +25,14 @@ def SubstanceTagMaker(FileType:str,NowPart:str,Type:int)->BeautifulSoup:
         #skillSeed-reward
         elif NowPart=='skillSeed':
             if Type==4:
-                _sSkillseedId=input(Fore.GREEN+'[INFO]Enter id of skillseed that you want to give:'+Fore.RESET)
-                _sSkillseedcount=input(Fore.GREEN+'[INFO]Enter count of skillseed that you want to give:'+Fore.RESET)
+                _sSkillseedId=input(Fore.GREEN+Content['Cross']['Input']['Input_SkillSeedID']+Fore.RESET)
+                _sSkillseedcount=input(Fore.GREEN+Content['Cross']['Input']['Input_SkillSeedCount']+Fore.RESET)
                 if checkconfig('AutoStr','Skill'):
                     _fFInd,_sSkillseedStr=getstr('name','AutoStr','Skill',_sSkillseedId)
                     if not _fFInd:
                         return SubstanceTagMaker(FileType,NowPart,Type)
                 else:
-                    _sSkillseedStr=input(Fore.GREEN+'[INFO]Enter str of skillseed that you want to give:'+Fore.RESET)
+                    _sSkillseedStr=input(Fore.GREEN+Content['Cross']['Input']['Input_SkillSeedStr']+Fore.RESET)
             else:
                 _sSkillseedId='-1'
                 _sSkillseedStr='Invalid'
@@ -41,13 +44,13 @@ def SubstanceTagMaker(FileType:str,NowPart:str,Type:int)->BeautifulSoup:
         #Other-reward 公版    
         else:
             if NowPart==TagNameList[Type]:
-                Id=input(Fore.GREEN+f'[INFO]Enter id of {TagNameList[Type]} that you want to give:'+Fore.RESET)
+                Id=input(Fore.GREEN+Content['Cross']['Input']['Input_ID'].replace('%TagName%',TagNameList[Type])+Fore.RESET)
                 if checkconfig('AutoStr',TagNameList[Type]):
                     _fFInd,Str=getstr('name','AutoStr',TagNameList[Type],Id)
                     if not _fFInd:
                         return SubstanceTagMaker(FileType,NowPart,Type)
                 else:
-                    Str=input(Fore.GREEN+f'[INFO]Enter str of {TagNameList[Type]} that you want to give:'+Fore.RESET)
+                    Str=input(Fore.GREEN+Content['Cross']['Input']['Input_Str'].replace('%TagName%',TagNameList[Type])+Fore.RESET)
             else:
                 Id='-1'
                 Str='Invalid'
@@ -60,8 +63,8 @@ def SubstanceTagMaker(FileType:str,NowPart:str,Type:int)->BeautifulSoup:
         FindKeyList=['name','works','name','category','name','genreNames','worksName','','']
         if NowPart=='musicDif':
             if Type==7:
-                Point=input(Fore.GREEN+'[INFO]Enter how many bonus step you want to give.'+Fore.RESET)
-                Id=input(Fore.GREEN+f'[INFO]Enter Trigger musicDif Id(ex:Basic->0).'+Fore.RESET)
+                Point=input(Fore.GREEN+Content['Cross']['Input']['Input_MapBonusPoint']+Fore.RESET)
+                Id=input(Fore.GREEN+Content['Cross']['Input']['Input_MapBonusMusicDif']+Fore.RESET)
                 Str=musicDif(int(Id))
             else:
                 Point='1'
@@ -71,8 +74,8 @@ def SubstanceTagMaker(FileType:str,NowPart:str,Type:int)->BeautifulSoup:
             return Tag
         elif NowPart=='musicLv':
             if Type==8:
-                Point=input(Fore.GREEN+'[INFO]Enter how many bonus step you want to give.'+Fore.RESET)
-                Lv=input(Fore.GREEN+f'[INFO]Enter Trigger Lv (ex:14+->14+).'+Fore.RESET)
+                Point=input(Fore.GREEN+Content['Cross']['Input']['Input_MapBonusPoint']+Fore.RESET)
+                Lv=input(Fore.GREEN+Content['Cross']['Input']['Input_MapBonusMusicLv']+Fore.RESET)
                 if Lv.endswith('+'):
                     Id=str(int(Lv[:-1])*2)
                     Str='ID_'+Id
@@ -90,14 +93,14 @@ def SubstanceTagMaker(FileType:str,NowPart:str,Type:int)->BeautifulSoup:
             return Tag
         elif NowPart=='skillCategory':
             if Type==3:
-                Point=input(Fore.GREEN+'[INFO]Enter how many bonus step you want to give.'+Fore.RESET)
-                Id=input(Fore.GREEN+f'[INFO]Enter Trigger {TagNameList[Type]} Id.'+Fore.RESET)
+                Point=input(Fore.GREEN+Content['Cross']['Input']['Input_MapBonusPoint']+Fore.RESET)
+                Id=input(Fore.GREEN+Content['Cross']['Input']['Input_MapBonusTriggerID'].replace('%TagName%',TagNameList[Type])+Fore.RESET)
                 if checkconfig('AutoStr',FileNameList[Type]):
                     _fFind,Str=getstr(FindKeyList[Type],'AutoStr',FileNameList[Type],Id)
                     if not _fFind:
                         return SubstanceTagMaker(FileType,NowPart,Type)
                 else:
-                    Str=input(Fore.GREEN+f'[INFO]Enter Trigger {TagNameList[Type]} Str.'+Fore.RESET)
+                    Str=input(Fore.GREEN+Content['Cross']['Input']['Input_MapBonusTriggerStr'].replace('%TagName%',TagNameList[Type])+Fore.RESET)
             else:
                 Point='1'
                 Id='-1'
@@ -107,14 +110,14 @@ def SubstanceTagMaker(FileType:str,NowPart:str,Type:int)->BeautifulSoup:
         
         elif NowPart=='musicGenre':
             if Type==5:
-                Point=input(Fore.GREEN+'[INFO]Enter how many bonus step you want to give.'+Fore.RESET)
-                Id=input(Fore.GREEN+f'[INFO]Enter Trigger {TagNameList[Type]} Id.'+Fore.RESET)
+                Point=input(Fore.GREEN+Content['Cross']['Input']['Input_MapBonusPoint']+Fore.RESET)
+                Id=input(Fore.GREEN+Content['Cross']['Input']['Input_MapBonusTriggerID'].replace('%TagName%',TagNameList[Type])+Fore.RESET)
                 if checkconfig('AutoStr',FileNameList[Type]):
                     _fFind,Str=getstr(FindKeyList[Type],'AutoStr',FileNameList[Type],Id)
                     if not _fFind:
                         return SubstanceTagMaker(FileType,NowPart,Type)
                 else:
-                    Str=input(Fore.GREEN+f'[INFO]Enter Trigger {TagNameList[Type]} Str.'+Fore.RESET)
+                    Str=input(Fore.GREEN+Content['Cross']['Input']['Input_MapBonusTriggerStr'].replace('%TagName%',TagNameList[Type])+Fore.RESET)
             else:
                 Point='1'
                 Id='-1'
@@ -124,14 +127,14 @@ def SubstanceTagMaker(FileType:str,NowPart:str,Type:int)->BeautifulSoup:
 
         elif NowPart=='musicWorks':
             if Type==6:
-                Point=input(Fore.GREEN+'[INFO]Enter how many bonus step you want to give.'+Fore.RESET)
-                Id=input(Fore.GREEN+f'[INFO]Enter Trigger {TagNameList[Type]} Id.'+Fore.RESET)
+                Point=input(Fore.GREEN+Content['Cross']['Input']['Input_MapBonusPoint']+Fore.RESET)
+                Id=input(Fore.GREEN+Content['Cross']['Input']['Input_MapBonusTriggerID'].replace('%TagName%',TagNameList[Type])+Fore.RESET)
                 if checkconfig('AutoStr',FileNameList[Type]):
                     _fFind,Str=getstr(FindKeyList[Type],'AutoStr',FileNameList[Type],Id)
                     if not _fFind:
                         return SubstanceTagMaker(FileType,NowPart,Type)
                 else:
-                    Str=input(Fore.GREEN+f'[INFO]Enter Trigger {TagNameList[Type]} Str.'+Fore.RESET)
+                    Str=input(Fore.GREEN+Content['Cross']['Input']['Input_MapBonusTriggerStr'].replace('%TagName%',TagNameList[Type])+Fore.RESET)
             else:
                 Point='1'
                 Id='-1'
@@ -141,14 +144,14 @@ def SubstanceTagMaker(FileType:str,NowPart:str,Type:int)->BeautifulSoup:
         
         else:
             if NowPart==TagNameList[Type]:
-                Point=input(Fore.GREEN+'[INFO]Enter how many bonus step you want to give.'+Fore.RESET)
-                Id=input(Fore.GREEN+f'[INFO]Enter Trigger {TagNameList[Type]} Id.'+Fore.RESET)
+                Point=input(Fore.GREEN+Content['Cross']['Input']['Input_MapBonusPoint']+Fore.RESET)
+                Id=input(Fore.GREEN+Content['Cross']['Input']['Input_MapBonusTriggerID'].replace('%TagName%',TagNameList[Type])+Fore.RESET)
                 if checkconfig('AutoStr',FileNameList[Type]):
                     _fFind,Str=getstr(FindKeyList[Type],'AutoStr',FileNameList[Type],Id)
                     if not _fFind:
                         return SubstanceTagMaker(FileType,NowPart,Type)
                 else:
-                    Str=input(Fore.GREEN+f'[INFO]Enter Trigger {TagNameList[Type]} Str.'+Fore.RESET)
+                    Str=input(Fore.GREEN+Content['Cross']['Input']['Input_MapBonusTriggerStr'].replace('%TagName%',TagNameList[Type])+Fore.RESET)
             else:
                 Point='1'
                 Id='-1'
@@ -157,20 +160,20 @@ def SubstanceTagMaker(FileType:str,NowPart:str,Type:int)->BeautifulSoup:
             return Tag
     elif FileType=='Map':
         if NowPart=='music':
-            _sCourseMusic=input(Fore.GREEN+'[INFO]Do you want to add 課題曲 in this area?(y/n)'+Fore.RESET).lower()
+            _sCourseMusic=input(Fore.GREEN+Content['Cross']['Input']['Input_MapHaveTaskTrack']+Fore.RESET).lower()
             if _sCourseMusic=='n':
                 Id='-1'
                 Str='Invalid'
                 isHard='false'
             else:
-                Id=input(Fore.GREEN+'[INFO]Enter the ID of music:'+Fore.RESET)
+                Id=input(Fore.GREEN+Content['Cross']['Input']['Input_TaskTrackID']+Fore.RESET)
                 if checkconfig('AutoStr','music'):
                     _fFInd,Str=getstr('name','AutoStr','music',Id)
                     if not _fFInd:
                         return SubstanceTagMaker(FileType,NowPart,Type)
                 else:
-                    Str=input(Fore.GREEN+'[INFO]Enter the Str of music:'+Fore.RESET)
-                    isHard=input(Fore.GREEN+'[INFO]Limit 課題曲 to Master mode only?(y/n)'+Fore.RESET).lower()
+                    Str=input(Fore.GREEN+Content['Cross']['Input']['Input_TaskTrackStr']+Fore.RESET)
+                    isHard=input(Fore.GREEN+Content['Cross']['Input']['Input_TaskTrackDifLock']+Fore.RESET).lower()
                     if isHard=='y':
                         isHard='true'
                     else:
@@ -185,3 +188,7 @@ def SubstanceTagMaker(FileType:str,NowPart:str,Type:int)->BeautifulSoup:
             else:
                 Str=input(Fore.GREEN+f'[INFO]Enter the Str of {NowPart}:'+Fore.RESET)
             return tagM('<'+NowPart+'Name><id>'+Id+'</id><str>'+Str+'</str><data /></'+NowPart+'Name>')
+
+def GetContent(data):
+    global Content
+    Content=data
